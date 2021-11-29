42f3017aef42c10d96d15e389a4945476e818e05
moonryc <70920813+moonryc@users.noreply.github.com>
2021-11-28 18:28:00 -0600

    form is now functional and code has been refactored

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,38 @@
-let buttonEl = document.querySelector("#save-task")
+let formEl = document.querySelector("#task-form")
 let tasksToDoEl = document.querySelector("#tasks-to-do")
 
-const createTaskHandler = () => {
+const taskFormHandler = (event) => {
+    event.preventDefault();
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeSelect = document.querySelector("select[name='task-type']").value;
+
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeSelect
+    }
+
+    createTaskEl(taskDataObj);
+
+}
+
+const createTaskEl = (taskDataObj) => {
+    //create list item
     let listItemEl = document.createElement("li");
-    listItemEl.textContent = "Task to do";
-    listItemEl.className = "task-item"
+    listItemEl.className = "task-item";
+
+    // create div to hold task info
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = `<h3>${taskDataObj.name}</h3> <span class="task-type">${taskDataObj.type}</span>`;
+
+    //append the div to the li
+    listItemEl.appendChild(taskInfoEl);
+
+    //append  the li to the ul
     tasksToDoEl.appendChild(listItemEl);
 }
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
